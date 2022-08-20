aeceb6d84a9b2a01bd031252339e01d7e260ff4d
Alexandre Visintainer <alexandrevisintainer@outlook.com>
2022-08-20 17:19:10 -0300

    Testing

--- components/Contact/index.jsx
+++ components/Contact/index.jsx
@@ -27,7 +27,7 @@ function Contact () {
         <h4 id="contact">Entre em contato</h4>
         <h2>Vamos tomar um café?</h2>
         <div className={styles.maps}>
-          <iframe width="100%" height="200" id="gmap_canvas" src="https://maps.google.com/maps?q=Boa%20Vista%20Centro&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+          <iframe width="100%" height="200" id="gmap_canvas" src="https://maps.google.com/maps?q=Boa%20Vista%20Centro&t=&z=13&ie=UTF8&iwloc=&output=embed" frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0"></iframe>
         </div>
         <form ref={form} onSubmit={sendEmail} className={styles.form}>
             <label >Nome</label>
